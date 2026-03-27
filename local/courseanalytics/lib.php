4142beb85b75815cd8e650060c7f7b96ebdbd78e
Alee24 <mettoalex@gmail.com>
2026-03-27 13:19:02 +0300

    fix: refine navigation link to ensure visibility as 'Course Analytics'

--- local/courseanalytics/lib.php
+++ local/courseanalytics/lib.php
@@ -11,32 +11,28 @@ defined('MOODLE_INTERNAL') || die();
 
 /**
  * Extend navigation for course.
- * This puts the Course Analytics link in the 'Reports' section of the course.
+ * This adds the Course Analytics link to reports container or directly to course node.
  *
  * @param global_navigation $navigation
  * @param stdClass $course
  * @param context_course $context
  */
 function local_courseanalytics_extend_navigation_course($navigation, $course, $context) {
-    if (has_capability('local/courseanalytics:view', $context)) {
-        // Find the 'Reports' node. In Moodle 4.x this is often 'reports' or 'coursereports'.
+    if (has_capability('local_courseanalytics:view', $context)) {
+        $url = new moodle_url('/local/courseanalytics/course.php', ['id' => $course->id]);
+        $name = 'Course Analytics';
+        
+        // Find 'reports' or 'coursereports'. In Moodle 4.x, 'reports' is the secondary nav container.
         $reportnode = $navigation->find('reports', navigation_node::TYPE_CONTAINER);
         if (!$reportnode) {
             $reportnode = $navigation->find('coursereports', navigation_node::TYPE_CONTAINER);
         }
-        
-        if (!$reportnode) {
-            $reportnode = $navigation;
-        }
 
-        $url = new moodle_url('/local/courseanalytics/course.php', ['id' => $course->id]);
-        $reportnode->add(
-            'Course Analytics',
-            $url,
-            navigation_node::TYPE_SETTING,
-            null,
-            'local_courseanalytics_report_link',
-            new pix_icon('i/report', '', 'moodle')
-        );
+        if ($reportnode) {
+            $reportnode->add($name, $url, navigation_node::TYPE_SETTING, null, 'local_courseanalytics_report_node', new pix_icon('i/report', '', 'moodle'));
+        } else {
+            // Add to the main course navigation if no reports container found
+            $navigation->add($name, $url, navigation_node::TYPE_SETTING, null, 'local_courseanalytics_report_direct', new pix_icon('i/report', '', 'moodle'));
+        }
     }
 }
